2b90fc7c0875add954fd6b640377e351e55eeae2
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-11 21:30:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_third_tier
       :names =>  {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :key3 => "",
+        :b_and_o_railroad => "",
         :key4 => ""
       },
       :rent_in_dollars => {
